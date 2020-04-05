ab7fbaf008563b8214ef2cb446ab6fa02a67e4de
nlemast <nlemaster47@gmail.com>
2020-04-05 16:09:33 -0400

    Create AboutPage component

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>
+        Culpa ullamco fugiat magna sunt enim. Fugiat sit non fugiat adipisicing
+        exercitation est quis ut anim. Aute amet occaecat voluptate pariatur
+        dolore.
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage

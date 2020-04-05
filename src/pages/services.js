a3c1c3ed816f2bfaf2fd564baa99a5c4f2d80a42
nlemast <nlemaster47@gmail.com>
2020-04-05 16:10:09 -0400

    Create servicesPage component

---
+++ src/pages/services.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+      <h1>Our Services</h1>
+      <p>
+        Culpa ullamco fugiat magna sunt enim. Fugiat sit non fugiat adipisicing
+        exercitation est quis ut anim. Aute amet occaecat voluptate pariatur
+        dolore.
+      </p>
+    </Layout>
+  )
+}
+
+export default ServicesPage

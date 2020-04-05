e098c71d35a7eb544e6e336d1ccbd8c452f0d60e
nlemast <nlemaster47@gmail.com>
2020-04-05 16:10:01 -0400

    Update Layout from default Gatsby CLI options

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div>
+      <Layout>
+        <SEO
+          description="This is a sample website for Gatsby crash course"
+          title="Home"
+        />
+        <h1>Welcome to my website</h1>
+        <p>This is a sample site for the Gatsby crash course</p>
+      </Layout>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
